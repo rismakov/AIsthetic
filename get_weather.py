07f6e64051557d61511e69265af6f0097ce441a7
Rebekkah Ismakov <rismakov@gmail.com>
2021-10-02 17:22:55 -0700

    Update get_weather.py
    if `BeautifulSoup` doesn't return anything, break out of loop

--- get_weather.py
+++ get_weather.py
@@ -89,6 +89,10 @@ def get_projected_weather(
         soup = BeautifulSoup(requests.get(weather_url).content, 'html.parser')
         sections = soup.find_all(True, {'class': re.compile(weather_info_class_regex)})
         
+        # if `BeautifulSoup` doesn't return anything, break out of loop:
+        if not sections:
+            break
+
         for x in sections:
             section_date = date(start_date.year, month, int(x.div.text))
             section_temp = int(x.span.text.replace('+', '').replace('°', ''))
