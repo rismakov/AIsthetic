0f55fc8c145bdfd0943be3a1dca6b3632d0c408e
Rebekkah Ismakov <rismakov@gmail.com>
2021-10-05 21:06:48 -0700

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -18,15 +18,18 @@ def image_processing(filepath):
     print('DEBUG', filepath)
     # load image file and import as a numpy array
     # type of `uint8` so values will be between 0-255
-    image = np.asarray(Image.open(filepath), dtype='uint8')        
-    # image = image.astype('uint8')    
+    # image = np.asarray(Image.open(filepath), dtype='uint8')     
+    # image = image.astype('uint8') 
 
-    st.image(image)
+    st.header(image.shape)
+    image_gray = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    # st.header(image_bgr.shape)
+    # st.image(image)
     # read the numpy arrays as color images in OpenCV
-    image_bgr = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    # image_bgr = cv2.imdecode(image, cv2.IMREAD_COLOR)
   
     # convert to grayscale to be used for training 
-    image_gray = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2GRAY)
+    # image_gray = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2GRAY)
 
     # resize the image to 100x100 pixels size
     image_100x100 = cv2.resize(image_gray, (100, 100))
