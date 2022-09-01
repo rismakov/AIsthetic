8cf5db7be88b042f7c8c22982ac539f28dd71068
Rebekkah Ismakov <rismakov@gmail.com>
2022-08-31 23:08:19 -0700

    Update count_closet.py

--- count_closet.py
+++ count_closet.py
@@ -1,18 +1,31 @@
-from outfit_utils import is_statement_item
+from typing import Tuple
+
+from outfit_utils import (
+    is_statement_item, filter_basic_items, filter_statement_items
+)
 
 from category_constants import ALL_CATEGORIES
 
-def get_basics_and_statements(items):
+
+def get_basics_and_statements(items: dict) -> Tuple[int, dict, dict]:
+    """
+    Parameters
+    ----------
+    items : dict
+        The clothing items as the values, with the clothing category the key.
+
+    Returns
+    -------
+        Tuple[int, dict, dict]
+    """
     count = 0
-    basics = []
-    statements = []
+    basics = {}
+    statements = {}
     for cat in items:
         count += len(items[cat])
-        for item in items[cat]:
-            if not is_statement_item(item): 
-                basics.append(item)
-            elif is_statement_item(item): 
-                statements.append(item)
+        basics[cat] = filter_basic_items(items[cat])
+        statements[cat] = filter_statement_items(items[cat])
+
     return count, basics, statements
 
 
@@ -21,7 +34,10 @@ def count_item_info(info_placeholder, items):
 
     info_placeholder.write(f"You have {count} items in your closet.")
 
-    basic_count, statement_count = len(basics), len(statements)
+    basic_count, statement_count = (
+        sum(len(x) for x in basics.values()),
+        sum(len(x) for x in statements.values()),
+    )
     total = basic_count + statement_count
     info_placeholder.write(
         f'- This includes {basic_count} basic pieces '
@@ -31,7 +47,7 @@ def count_item_info(info_placeholder, items):
     info = '- '
     for cat in ALL_CATEGORIES:
         info += f'{len(items[cat])} {cat}, '
-    
+
     info_placeholder.write(f'{info[:-2]}.')
 
 
@@ -55,7 +71,7 @@ def count_outfits(info_placeholder, outfits, items=[]):
 
     if items:
         count_item_info(info_placeholder, items)
-    
+
     info_placeholder.write(
         f"You have {outfit_count:,} unique outfit combinations ("
         f"{outfit_count_2:,} outfit combinations if only calculating distinct"
