f654ed2670298016e863f6f88e94ac11e67365ee
Rebekkah Ismakov <rismakov@gmail.com>
2022-08-31 23:05:44 -0700

    Update outfit_utils.py

--- outfit_utils.py
+++ outfit_utils.py
@@ -1,102 +1,26 @@
-import random
+from typing import List
 
-from category_constants import TAGS
-
-CARRY_ON = {
-    'outerwear': 2,
-    'shoes': 2,
-}
-
-SUITCASE = {
-    'outerwear': 3,
-    'shoes': 3,
-}
-
-AMOUNT_MAPPINGS = {
-    'small carry-on suitcase': CARRY_ON,
-    'medium suitcase': SUITCASE,
-    'entire closet': {},
-}
+from category_constants import OCCASIONS, SEASONS, TAGS
 
 
-def is_statement_item(item):
+def is_statement_item(item: str) -> bool:
     return TAGS['style']['Statement'] in item
 
 
-def filter_basic_items(items):
+def filter_basic_items(items: List[str]) -> List[str]:
     return [item for item in items if not is_statement_item(item)]
 
 
-def filter_statement_items(items):
+def filter_statement_items(items: List[str]) -> List[str]:
     return [item for item in items if is_statement_item(item)]
 
 
-def add_to_chosen_basic_items(filepaths, basic_items, cat, occasion, season):
-    season_occasion_items = filter_items(
-        filepaths[cat], occasions=[occasion], seasons=[season],
-    )
-    season_occasion_basic_items = filter_basic_items(season_occasion_items)
-
-    if season_occasion_basic_items:
-        basic_items += [random.choice(season_occasion_basic_items)]
-
-    return basic_items
-
-
-def filter_items_based_on_amount(
-    filepaths: dict, amount: str, occasions: list, seasons: list
-):
-    """Choose only a specific amount of possible items from options.
-
-    Make sure at least one basic item is available for each cateogry type.
-    Also, make sure at least one item is available for each occation type.
-
-    Parameters
-    ----------
-    filepaths : dict
-    amount : str
-    occasions : list
-    seasons : list
-
-    Returns
-    -------
-    dict
-    """
-    for cat, k in AMOUNT_MAPPINGS[amount].items():
-        # Add basic item to each season/occasion pair - so at least one item is
-        # able to match to everything:
-        basic_items = []
-        for occasion in occasions:
-            for season in seasons:
-                # If no basic item included for this season/occasion pair, add 
-                # one in:
-                if not filter_items(
-                    basic_items, occasions=[occasion], seasons=[season],
-                ):
-                    basic_items = add_to_chosen_basic_items(
-                        filepaths, basic_items, cat, occasion, season
-                    )
-
-        subset = basic_items 
-
-        # Add as many more items as allowed for amount type:
-        options = [x for x in filepaths[cat] if x not in basic_items]
-        items_left = max(0, k - len(basic_items))
-        if options:
-            subset += random.choices(options, k=items_left)
-
-        filepaths[cat] = subset
-    return filepaths
-
-
-def filter_items(
-    items: list, 
-    seasons: list=list(TAGS['season'].keys()), 
-    occasions: list=list(TAGS['occasion'].keys())
+def filter_category_of_items(
+    items: list, seasons: list = SEASONS, occasions: list = OCCASIONS
 ) -> list:
     """Filter list to only include items with specfic tags.
-    
-    Items in list must include at least one of the `seasons` tags AND at least 
+
+    Items in list must include at least one of the `seasons` tags AND at least
     one of the `occasions` tags.
 
     Parameters
@@ -112,7 +36,7 @@ def filter_items(
     """
     season_tags = [TAGS['season'][season] for season in seasons]
     occasion_tags = [TAGS['occasion'][occasion] for occasion in occasions]
-    
+
     return [
         item for item in items if (
             any(tag in item for tag in season_tags)
@@ -120,7 +44,10 @@ def filter_items(
         )
     ]
 
-def filter_appropriate_outfits(outfits, seasons, occasions):
+
+def filter_appropriate_outfits(
+    outfits: list, seasons: list, occasions: list
+) -> list:
     return [
         outfit for outfit in outfits if (
             any(season in outfit['tags']['season'] for season in seasons)
@@ -128,10 +55,15 @@ def filter_appropriate_outfits(outfits, seasons, occasions):
         )
     ]
 
-def filter_items_in_all_categories(
-    filepaths: dict, seasons: list, occasions: list
+
+def filter_appropriate_items(
+    items: dict, seasons: list, occasions: list
 ) -> dict:
     return {
-        cat: filter_items(filepaths[cat], seasons, occasions) 
-        for cat in filepaths
-    } 
+        cat: filter_category_of_items(items[cat], seasons, occasions)
+        for cat in items
+    }
+
+
+def filter_outfits_on_item(outfits, cat, item):
+    return [outfit for outfit in outfits if outfit[cat] == item]
