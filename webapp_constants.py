346e6dc202c60381e6d6f89d3cae6e4bf3fc53c8
Rebekkah Ismakov <rismakov@gmail.com>
2022-09-07 19:50:25 -0700

    Create webapp_constants.py

---
+++ webapp_constants.py
@@ -0,0 +1,6 @@
+RANDOM_OPTION = "Select a random outfit combination from closet"
+INSPO_OPTION = "Select an outfit based on an inspo-photo"
+TRIP_OPTION = "Plan a set of outfits for a trip"
+VIEW_OPTION = "View all clothing articles in closet"
+
+OPTIONS = [RANDOM_OPTION, INSPO_OPTION, TRIP_OPTION, VIEW_OPTION]
