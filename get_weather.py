e589e255aca34e14a11f2ac5bd3e93896bbfd653
Rebekkah Ismakov <rismakov@gmail.com>
2022-11-05 00:03:01 -0700

    Update get_weather.py

--- get_weather.py
+++ get_weather.py
@@ -1,27 +1,28 @@
 import re
-import requests 
+import requests
 
 from bs4 import BeautifulSoup
 from datetime import date, timedelta
 
 MONTH_MAPPING = [
-    'january', 
-    'feburary', 
-    'march', 
-    'april', 
-    'may', 
-    'june', 
-    'july', 
-    'august', 
-    'september', 
-    'october', 
-    'november', 
+    'january',
+    'feburary',
+    'march',
+    'april',
+    'may',
+    'june',
+    'july',
+    'august',
+    'september',
+    'october',
+    'november',
     'december',
 ]
 
 COUNTRY_MAPPING = {'england': 'united_kingdom'}
 
 URL = "https://world-weather.info/forecast/{}/{}/{}-{}/"
+
 
 # http://www.city-data.com/forum/weather/2237356-what-do-people-consider-above-warm-3.html
 def get_weather_type(temp: int, weather: str) -> str:
@@ -43,7 +44,7 @@ def get_weather_type(temp: int, weather: str) -> str:
     if temp > 85:
         return 'Hot'
     # if between 80-85, or between 70-80 and not cloudy
-    if (temp > 80) or (temp > 70 and (not 'cloud' in weather.lower())):
+    if (temp > 80) or (temp > 70 and ('cloud' not in weather.lower())):
         return 'Warm'
     # if between 65-70, or between 70-80 and cloudy
     if temp > 65:
@@ -56,7 +57,7 @@ def get_weather_type(temp: int, weather: str) -> str:
 
 
 def get_projected_weather(
-    city: str, country: str, start_date, end_date
+    start_date, end_date, city: str, country: str
 ) -> dict:
     """
     Parameters
@@ -73,7 +74,7 @@ def get_projected_weather(
     """
     weather_info_class_regex = 'ww-month-week(end|days) (fore(a|)cast)(-statistics|)'
     weather_class_regex = 'icon-weather wi d\d{3} tooltip'
-    
+
     date_temps = {}
     temps = []
     weathers = []
@@ -88,7 +89,7 @@ def get_projected_weather(
         print(f'Scraping {weather_url}...')
         soup = BeautifulSoup(requests.get(weather_url).content, 'html.parser')
         sections = soup.find_all(True, {'class': re.compile(weather_info_class_regex)})
-        
+
         # If `BeautifulSoup` doesn't return anything, break out of loop
         if not sections:
             break
