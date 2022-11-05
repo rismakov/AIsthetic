8a096e1dc05f12b8ae3aad8c200ee83996755fde
Rebekkah Ismakov <rismakov@gmail.com>
2022-11-05 00:04:33 -0700

    Update count_closet.py

--- count_closet.py
+++ count_closet.py
@@ -1,19 +1,16 @@
 from typing import Tuple
 
-from outfit_utils import (
-    is_statement_item, filter_basic_items, filter_statement_items
-)
+from outfit_utils import get_item_name, is_item_of_type_style, filter_items_by_style
 
 from category_constants import ALL_CATEGORIES
 
 
-def get_basics_and_statements(items: dict) -> Tuple[int, dict, dict]:
+def get_basics_and_statements(closet) -> Tuple[int, dict, dict]:
     """
     Parameters
     ----------
-    items : Dict[str, list]
-        The key is the category (eg 'tops', 'bottoms', etc) and the values are
-        the list of items.
+    closet : create_closet.Closet
+        The closet, including `items` and `items_tags`.
 
     Returns
     -------
@@ -22,16 +19,16 @@ def get_basics_and_statements(items: dict) -> Tuple[int, dict, dict]:
     count = 0
     basics = {}
     statements = {}
-    for cat in items:
-        count += len(items[cat])
-        basics[cat] = filter_basic_items(items[cat])
-        statements[cat] = filter_statement_items(items[cat])
+    for cat, cat_items in closet.items.items():
+        count += len(closet.items[cat])
+        basics[cat] = filter_items_by_style(cat_items, closet.items_tags[cat], 'Basic')
+        statements[cat] = filter_items_by_style(cat_items, closet.items_tags[cat], 'Statement')
 
     return count, basics, statements
 
 
-def count_item_info(info_placeholder, items):
-    count, basics, statements = get_basics_and_statements(items)
+def count_item_info(info_placeholder, closet):
+    count, basics, statements = get_basics_and_statements(closet)
 
     info_placeholder.write(f"You have {count} items in your closet.")
 
@@ -47,32 +44,35 @@ def count_item_info(info_placeholder, items):
     )
     info = '- '
     for cat in ALL_CATEGORIES:
-        info += f'{len(items[cat])} {cat}, '
+        info += f'{len(closet.items[cat])} {cat}, '
 
     info_placeholder.write(f'{info[:-2]}.')
 
 
-def count_outfits(info_placeholder, outfits, items_tags, items=[]):
-    outfit_count = len(outfits)
+def count_outfits(info_placeholder, closet):
+    outfit_count = len(closet.outfits)
     # count non-statement outfits
     basic_outfit_count = sum(
-        not outfit['tags']['is_statement'] for outfit in outfits
+        not outfit['tags']['is_statement'] for outfit in closet.outfits
     )
     # count distinct statement pieces in statement outfits
     statement_outfits = [
-        outfit for outfit in outfits if outfit['tags']['is_statement']
+        outfit for outfit in closet.outfits if outfit['tags']['is_statement']
     ]
     unique_statement_pieces = []
     for outfit in statement_outfits:
         for cat, item in outfit.items():
-            if is_statement_item(item, items_tags[cat][item]):
+            if is_item_of_type_style(
+                closet.items_tags[cat][
+                    get_item_name(item, closet.is_user_closet)
+                ], 'Statement'
+            ):
                 unique_statement_pieces.append(item)
 
     unique_statement_piece_count = len(set(unique_statement_pieces))
     outfit_count_2 = basic_outfit_count + unique_statement_piece_count
 
-    if items:
-        count_item_info(info_placeholder, items)
+    count_item_info(info_placeholder, closet)
 
     info_placeholder.write(
         f"You have {outfit_count:,} unique outfit combinations ("
