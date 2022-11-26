6a3c23ab2f51b6cbee5ea500b1e279a777c2c9ab
Rebekkah Ismakov <rismakov@gmail.com>
2022-11-26 00:00:47 -0800

    Update webapp_constants.py

--- webapp_constants.py
+++ webapp_constants.py
@@ -1,7 +1,7 @@
-CAMERA_OPTION = "Snap image of clothing item you want to purchase"
 RANDOM_OPTION = "Select a random outfit combination from closet"
 INSPO_OPTION = "Select an outfit based on an inspo-photo"
 TRIP_OPTION = "Plan a set of outfits for a trip"
 VIEW_OPTION = "View all clothing articles in closet"
+CAMERA_OPTION = "Snap image of clothing item you want to purchase"
 
-OPTIONS = [CAMERA_OPTION, RANDOM_OPTION, INSPO_OPTION, TRIP_OPTION, VIEW_OPTION]
+OPTIONS = [RANDOM_OPTION, INSPO_OPTION, TRIP_OPTION, VIEW_OPTION, CAMERA_OPTION]
